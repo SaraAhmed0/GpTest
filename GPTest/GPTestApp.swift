21f5a3cba7ab11f61eb975f8f251487f46eb2117
Danya <sara.ahmad2501@gmail.com>
2022-12-14 14:58:01 +0300

    GP Test

---
+++ GPTest/GPTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTestApp.swift
+//  GPTest
+//
+//  Created by Sara AlMezeini on 20/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct GPTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
